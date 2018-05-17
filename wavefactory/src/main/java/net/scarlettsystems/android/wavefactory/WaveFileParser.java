1b79208128185d218199589424820262eb23c591
shanescarlett <shane.choo+github@gmail.com>
2018-05-17 19:59:13 +0900

    Add reading of bits per sample

--- wavefactory/src/main/java/net/scarlettsystems/android/wavefactory/WaveFileParser.java
+++ wavefactory/src/main/java/net/scarlettsystems/android/wavefactory/WaveFileParser.java
@@ -1,15 +1,19 @@
 package net.scarlettsystems.android.wavefactory;
 
+@SuppressWarnings("unused")
 class WaveFileParser
 {
-	private char[] formatChars = {'f','m','t',' '};
-	private char[] dataChars = {'d','a','t','a'};
+	private int mChannelCount = 0;
 	private long mSampleRate = 0;
+	private int mBitsPerSample = 0;
 	private long mDataLength = 0;
 	private int mDataStartIndex = 0;
 
 	WaveFileParser(byte[] bytes)
 	{
+		char[] formatChars = {'f','m','t',' '};
+		char[] dataChars = {'d','a','t','a'};
+
 		for(int c = 0; c < bytes.length - dataChars.length; c++)
 		{
 			if(bytes[c] == formatChars[0]
@@ -17,7 +21,9 @@ class WaveFileParser
 					&& bytes[c+2] == formatChars[2]
 					&& bytes[c+3] == formatChars[3])
 			{
+				mChannelCount = getUInt16(bytes[c+10], bytes[c+11]);
 				mSampleRate = getUInt32(bytes[c+16], bytes[c+17], bytes[c+18], bytes[c+19]);
+				mBitsPerSample = getUInt16(bytes[c+34], bytes[c+35]);
 			}
 
 			if(bytes[c] == dataChars[0]
@@ -31,6 +37,13 @@ class WaveFileParser
 		}
 	}
 
+	private int getUInt16(byte small, byte big)
+	{
+		int value = small & 0xFF;
+		value |= (big << 8) & 0xFFFF;
+		return value;
+	}
+
 	private long getUInt32(byte smallest, byte small, byte big, byte biggest)
 	{
 		long value = smallest & 0xFF;
@@ -40,9 +53,19 @@ class WaveFileParser
 		return value;
 	}
 
+	int getChannelCount()
+	{
+		return mChannelCount;
+	}
+
 	long getSampleRate()
 	{
 		return mSampleRate;
+	}
+
+	int getBitsPerSample()
+	{
+		return mBitsPerSample;
 	}
 
 	long getDataLength()
