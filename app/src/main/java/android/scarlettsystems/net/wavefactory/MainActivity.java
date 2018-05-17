136759d3c0808e1b2549fba5d00e68ebbc269c0c
shanescarlett <shane.choo+github@gmail.com>
2018-05-17 19:58:27 +0900

    Add more sample charts

--- app/src/main/java/android/scarlettsystems/net/wavefactory/MainActivity.java
+++ app/src/main/java/android/scarlettsystems/net/wavefactory/MainActivity.java
@@ -21,14 +21,67 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		float[] wave = WaveFactory.pcmToFloat(WaveFactory.getSawtoothWave(1000, 0.1f, 44100, 0));
-		LineChart chart = findViewById(R.id.chart);
+		drawSineWave();
+		drawSquareWave();
+		drawTriangularWave();
+		drawSawtoothWave();
+	}
+
+	private void drawSineWave()
+	{
+		float[] wave = WaveFactory.pcmToFloat(WaveFactory.getSineWave(200, 0.01f, 44100, 0));
+		LineChart chart = findViewById(R.id.sine);
 		List<Entry> entries = new ArrayList<>();
 		for(int c = 0; c < wave.length; c++)
 		{
 			entries.add(new Entry((float)c / 44100f, wave[c]));
 		}
-		LineDataSet dataSet = new LineDataSet(entries, "Wave");
+		LineDataSet dataSet = new LineDataSet(entries, "Sine Wave");
+		LineData lineData = new LineData(dataSet);
+		chart.setData(lineData);
+		chart.invalidate();
+	}
+
+	private void drawSquareWave()
+	{
+		float[] wave = WaveFactory.pcmToFloat(WaveFactory.getSquareWave(200, 0.01f, 44100, 0));
+		LineChart chart = findViewById(R.id.square);
+		List<Entry> entries = new ArrayList<>();
+		for(int c = 0; c < wave.length; c++)
+		{
+			entries.add(new Entry((float)c / 44100f, wave[c]));
+		}
+		LineDataSet dataSet = new LineDataSet(entries, "Square Wave");
+		LineData lineData = new LineData(dataSet);
+		chart.setData(lineData);
+		chart.invalidate();
+	}
+
+	private void drawTriangularWave()
+	{
+		float[] wave = WaveFactory.pcmToFloat(WaveFactory.getTriangularWave(200, 0.01f, 44100, 0));
+		LineChart chart = findViewById(R.id.triangular);
+		List<Entry> entries = new ArrayList<>();
+		for(int c = 0; c < wave.length; c++)
+		{
+			entries.add(new Entry((float)c / 44100f, wave[c]));
+		}
+		LineDataSet dataSet = new LineDataSet(entries, "Triangular Wave");
+		LineData lineData = new LineData(dataSet);
+		chart.setData(lineData);
+		chart.invalidate();
+	}
+
+	private void drawSawtoothWave()
+	{
+		float[] wave = WaveFactory.pcmToFloat(WaveFactory.getSawtoothWave(200, 0.01f, 44100, 0));
+		LineChart chart = findViewById(R.id.sawtooth);
+		List<Entry> entries = new ArrayList<>();
+		for(int c = 0; c < wave.length; c++)
+		{
+			entries.add(new Entry((float)c / 44100f, wave[c]));
+		}
+		LineDataSet dataSet = new LineDataSet(entries, "Sawtooth Wave");
 		LineData lineData = new LineData(dataSet);
 		chart.setData(lineData);
 		chart.invalidate();
